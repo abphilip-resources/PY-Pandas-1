cc9ec7ccc857f23c5a8ed2044e825bfa0b22c73d
allenalvin333 <allenalvin333@gmail.com>
2025-01-15 15:19:48 +1100

    Drop Duplicate Rows

---
+++ Data Cleaning/6.py
@@ -0,0 +1,7 @@
+# 2882
+# https://leetcode.com/problems/drop-duplicate-rows
+
+import pandas as pd
+
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    return customers.drop_duplicates(subset='email', keep='first')
